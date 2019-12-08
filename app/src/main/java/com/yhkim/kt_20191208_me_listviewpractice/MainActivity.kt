7d55471953906668ffb763837117c87eeee6baf5
kvoyant <kvoyant@gmail.com>
2019-12-08 14:44:43 +0900

    사용자 목록을 배열데 추가 - 별개 함수로 따로 빼냄

--- app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/MainActivity.kt
@@ -8,6 +8,10 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    val studentList = ArrayList<User>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,5 +37,23 @@ class MainActivity : AppCompatActivity() {
         val tempUser4 = User("아이유")
         Log.d("사용자 주소",tempUser4.address)//거주지만 모름
 
+//        학생목록 추가
+        addStudents()
+
     }
+
+    fun addStudents() {
+        studentList.add(User("계석준","서울시 도봉구", false))
+        studentList.add(User("김미현","경기도 군포시", true))
+        studentList.add(User("김영호","서울시 은평구", false))
+        studentList.add(User("노혜진","서울시 강동구", true))
+        studentList.add(User("류찬울","서울시 어딘가", false))
+        studentList.add(User("양성심","서울시 관악구", true))
+        studentList.add(User("이규현","서울시 도봉구", false))
+        studentList.add(User("이수정","서울시 고양구", true))
+        studentList.add(User("차순혁","서울시 구로구", false))
+        studentList.add(User("황연아","경기도 성남시", true))
+        studentList.add(User("조경진","서울시 은평구", false))
+    }
+
 }
--- app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/datas/User2.kt
+++
@@ -1,4 +0,0 @@
-package com.yhkim.kt_20191208_me_listviewpractice.datas
-
-class User2 {
-}
