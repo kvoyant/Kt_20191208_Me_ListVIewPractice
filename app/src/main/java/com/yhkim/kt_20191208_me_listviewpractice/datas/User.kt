1ddf161b91cd4dc503c7e7cd1f052034dd75c78f
kvoyant <kvoyant@gmail.com>
2019-12-08 14:13:47 +0900

    생성시 추가동작 : init, 일반 함수 정보 찍기 추가

--- app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/datas/User.kt
+++ app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/datas/User.kt
@@ -13,6 +13,10 @@ class User( inputName:String, inputAddress:String, isWomenOk:Boolean ) {
         Log.d("생성자호출",inputName)//생성자 호출시 부가 동작 실행
     }
 
+    fun printUserInfo() {
+        Log.d("사용자 정보", "이름 : ${name}, 거주지 : ${address}, 여성인가? ${isWomen}")
+    }
+
     constructor() : this("미정" , "거주지 불명", false)
 
     constructor(name:String) : this(name, "거주지만 모름", true)
