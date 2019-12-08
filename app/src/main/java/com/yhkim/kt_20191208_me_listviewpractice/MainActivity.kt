10a1e335c9f42fb812741120e41274d03f82f1d3
kvoyant <kvoyant@gmail.com>
2019-12-08 14:10:34 +0900

    보조생성자 추가 연습

--- app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/MainActivity.kt
@@ -19,12 +19,19 @@ class MainActivity : AppCompatActivity() {
 
         val tempUser = User(inputName = "김영호", inputAddress = "서울시 은평구", isWomenOk = true)
 
-        Log.d("tempUser","${tempUser.name}")
         Log.d("사용자 이름","${tempUser2.name}")
 
 //        람다형식 간소화 (예)
-//        okBtn.setOnCLickListner{}
+//        okBtn.setOnCLickListner {} //중괄호만 쓰면 람다 형식( 후행 클로저 )
 //        okBtn.setOnClicKListner(object : HostCallbacks() {
 //        })
+
+        Log.d("사용자 주소","${tempUser.address}")
+        var tempUser3 = User()
+        Log.d("사용자 주소","${tempUser3.address}")
+
+        val tempUser4 = User("아이유")
+        Log.d("사용자 주소",tempUser4.address)//거주지만 모름
+
     }
 }
--- app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/datas/User.kt
+++ app/src/main/java/com/yhkim/kt_20191208_me_listviewpractice/datas/User.kt
@@ -1,9 +1,19 @@
 package com.yhkim.kt_20191208_me_listviewpractice.datas
 
+import android.util.Log
+import java.util.*
+
 class User( inputName:String, inputAddress:String, isWomenOk:Boolean ) {
 
     var name = inputName
     var address = inputAddress
     var isWomen = isWomenOk
 
+    init {
+        Log.d("생성자호출",inputName)//생성자 호출시 부가 동작 실행
+    }
+
+    constructor() : this("미정" , "거주지 불명", false)
+
+    constructor(name:String) : this(name, "거주지만 모름", true)
 }
